98e0fb215223e4c8cacef630bb643666a19adfb3
zl <1019764004@qq.com>
2024-12-12 17:59:59 +0800

    update createDID

--- app/components/home/CreateDID.tsx
+++ app/components/home/CreateDID.tsx
@@ -2,11 +2,58 @@
 
 import { paytoneOne } from '@/app/ui/fonts';
 import Image from 'next/image';
-import { useAccount } from 'wagmi';
+import { useAccount, useSignMessage } from 'wagmi';
+import { useConnectModal } from '@rainbow-me/rainbowkit';
+import React, { useState } from 'react';
 
 export default function CreateDID() {
     const { isConnected, address } = useAccount();
-    const currentAddress = isConnected ? address.slice(0,6) + '...' + address.slice(-4) : '0x0000...0000'
+    const currentAddress = isConnected ? address.slice(0, 6) + '...' + address.slice(-4) : '0x0000...0000'
+
+    const { signMessage, data, isPending, isError, isSuccess } = useSignMessage()
+    const message = "hello world"
+    const { openConnectModal } = useConnectModal();
+    const url = 'http://119.147.213.61:38082/did'
+    const [did, setDID] = useState<string | null>(null);
+
+    const getSigMsg = async (address) => {
+        
+    }
+
+    const createDID = async (sig: string) => {
+        // const response = await fetch(url, {
+        //     method: "POST",
+        //     headers: {
+        //         'Content-Type': 'application/json;charset=utf-8'
+        //     },
+        //     body: JSON.stringify({address, sig}),
+        // })
+        // .then(response => {
+        //     const data = response.json()
+        //     return data.did
+        // })
+        // .then(e => {
+        //     console.log(e)
+        // })
+        // .catch(err => {
+        //     console.error(err)
+        // })
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                },
+                body: JSON.stringify({ address, sig }),
+            });
+
+            const data = await response.json();
+            console.log(data.did);
+        } catch (err) {
+            console.error('Error creating DID:', err);
+        }
+    }
+
     return (
         <div className='mt-[97px] flex justify-center bg-dark' >
             <div className="border-[3px] rounded-[11px] border-white px-[38px] py-[26px] bg-gradient-to-r from-[#064E33] to-[#214177]">
@@ -22,7 +69,24 @@ export default function CreateDID() {
                     <div className='text-[16px] text-white leading-[30px]'>Gas Fee</div>
                     <div className='text-[16px] text-white leading-[30px]'>0.00 MEMO</div>
                 </div>
-                <div className='w-full py-[9px] text-center text-[16px] rounded-full font-bold bg-[#05F292]  mt-[10px]'>Create DID</div>
+                {/*  Create DID button */}
+                <div className='w-full py-[9px] text-center text-[16px] rounded-full font-bold bg-[#05F292]  mt-[10px]'>
+                    {isConnected ? (
+                        <div>
+                            <button onClick={() => signMessage({ message })} disabled={isPending}>
+                            Create DID
+                            </button>
+                            {isSuccess && <div>Signature: {data}</div>}
+                            {isError && <div>Error signing message</div>}
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={openConnectModal}>
+                            Create DID
+                            </button>
+                        </div>
+                    )}
+                </div>
                 <div className='text-[10px] leading-[30px] te3xt-white mt-[16px] text-white text-center'>Gas fee represents the transaction fee for creating a DID, which is free at this stage.</div>
             </div>
         </div>
