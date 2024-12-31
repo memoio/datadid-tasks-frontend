061883b45b0bbbd8d1df012295284497daa10a6a
hk <635207979@qq.com>
2024-12-31 11:04:46 +0800

    add projects

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
   return (
     <div>
-      <main className="bg-[#051610] px-[120px] sm:px-[100px] md:px-[120px] lg:px-[160px] xl:px-[224px] py-[20px] sm:py-[25px] md:py-[30px] lg:py-[35px] xl:px-[40px] min-h-[100vh]">
+      <main className="bg-[#051610] px-[120px] sm:px-[100px] md:px-[120px] lg:px-[160px] xl:px-[224px] py-[20px] sm:py-[25px] md:py-[30px] lg:py-[35px] xl:py -[40px] min-h-[100vh]">
         {/* <FlagProvider> */}
         <Navbar />
         {
