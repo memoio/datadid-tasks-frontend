5f1a92dcd5a9166f02d9a95ecf6dce0e36368705
hk <635207979@qq.com>
2025-03-12 10:37:12 +0800

    add oat

--- app/components/Bonus/Reward.tsx
+++ app/components/Bonus/Reward.tsx
@@ -4,20 +4,72 @@ import Image from 'next/image';
 import { paytoneOne } from '@/app/ui/fonts';
 import { useAccount } from 'wagmi';
 import { useAuth } from '@/app/lib/context/AuthContext';
-
+import { use, useEffect, useState } from 'react';
+import axios from 'axios';
+import { API_URL } from '../config/config';
+import { useAction } from '@/app/lib/context/ActionContext';
 
 export default function Reward() {
     const { isConnected } = useAccount();
-    const { isExist } = useAuth();
+    const { address } = useAccount();
+    const { uidInfo } = useAuth();
+    const { setPointUpdate } = useAction();
+
+    const [OATCount, setOATCount] = useState(0);
 
     const handVerifyOAT = async () => {
         if (isConnected) {
-            const actionId = 85;
-        
-            
+            const actionId = 87;
+            // 根据OATCount次数，多次调用API_URL.AIRDROP_RECORD_ADD
+            for (let i = 0; i < OATCount; i++) {
+                const respond = await axios.post(API_URL.AIRDROP_RECORD_ADD, {
+                    "action": actionId
+                }, {
+                    headers: {
+                        "accept": "application/hal+json",
+                        "Content-Type": "application/json",
+                        "uid": uidInfo?.uid,
+                        "token": uidInfo?.token
+                    }
+                });
+                console.log("OAT:", respond.data);
+            }
+
+
+            if (address) {
+                await fetch(`/api/${encodeURIComponent(address)}`, {
+                    method: 'POST',
+                });
+            } else {
+                console.error('Address is undefined');
+            }
+
+            setPointUpdate(true)
             return;
         }
     }
+
+    useEffect(() => {
+        if (isConnected) {
+            if (address) {
+                fetch(`/api/${encodeURIComponent(address)}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(`Address exists ${data.count} times in the database.`);
+                        setOATCount(data.count);
+                    })
+                    .catch(error => {
+                        console.error('Fetch error:', error);
+                    });
+            } else {
+                console.error('Address is undefined');
+            }
+        }
+    }, [address, isConnected, handVerifyOAT])
+
+    const { isExist } = useAuth();
+
+
     return (
         <div
             id="reward-section"
@@ -48,18 +100,26 @@ export default function Reward() {
                                 rewards. Please make sure that the current wallet address is
                                 consistent with the address for receiving OAT.
                             </div>
+                            <div className="text-[16px] text-white text-center sm:text-left">
+                                OAT Count: {OATCount}
+                            </div>
                         </div>
                     </div>
                     {/* Right Section */}
                     <div
                         className="bg-[#0079F2] text-white text-[18px] font-bold text-center px-[25px] py-[20px] hover:bg-[#04D582] hover:scale-105 transition-transform duration-300 rounded-[10px]"
-                        style={{ pointerEvents: 'none', opacity: '0.5' }}
+                        style={{ pointerEvents: (OATCount > 0) ? 'auto' : 'none', opacity: (OATCount > 0) ? 1 : 0.5 }} // 
                         onClick={() => handVerifyOAT()}
                     >
-                        Comming Soon
+                        {
+                            (OATCount > 0) ?
+                                "Verify OAT" :
+                                "No OAT"
+                        }
+
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
