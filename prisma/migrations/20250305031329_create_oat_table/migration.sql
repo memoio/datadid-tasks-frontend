87078a2af4d54e509cb355144cf022a250c64968
hk <635207979@qq.com>
2025-03-18 17:33:31 +0800

    fix oat

--- prisma/migrations/20250305031329_create_oat_table/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "OAT" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "address" TEXT NOT NULL,
-    "projectId" TEXT NOT NULL,
-    "status" BOOLEAN NOT NULL DEFAULT false
-);
--- prisma/migrations/20250305083339_fix/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[address,projectId]` on the table `OAT` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "OAT_address_projectId_key" ON "OAT"("address", "projectId");
