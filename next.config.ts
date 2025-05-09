a09470de3d999c83a832c80db66a6000dd880b43
hk <635207979@qq.com>
2025-05-09 11:36:28 +0800

    fix config

--- next.config.ts
+++ next.config.ts
@@ -4,29 +4,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    DID_SERVER_URL: (() => {
-      if (process.env.NODE_ENV === 'development') {
-        return 'https://didapi.memolabs.org/did';
-      } else if (process.env.NODE_ENV === 'test') {
-        return 'https://testdidapi.memolabs.org/did';
-      } else {
-        return 'https://didapi.memolabs.org/did';
-      }
-    })(),
-
-    AIRDROP_BACKEND_URL: (() => {
-      if (process.env.NODE_ENV === 'development') {
-        return 'https://apapitest.memoscan.org/api';
-      } else {
-        return 'https://apapi.memoscan.org/api';
-      }
-    })(),
-
     BACKEND_URL: (() => {
        if (process.env.NODE_ENV === 'development') {
         return 'http://localhost:8080';
       } else {
-        return 'https://data-be.metamemo.one';
+        return 'https://data-be.metamemo.one/v1';
       }
     })()
   }
